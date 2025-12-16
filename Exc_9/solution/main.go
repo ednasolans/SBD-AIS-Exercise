55b655e9ddd2d905b4056fc796e7559d6c63fdcf
ednasolans <edna.solans@gmail.com>
2025-12-16 16:02:22 +0100

    Exc_9 solution

--- Exc_9/solution/main.go
+++ Exc_9/solution/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"exc9/mapred"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 // Main function
 func main() {
 	// todo read file
+	data, err := os.ReadFile("solution/res/meditations.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// todo run your mapreduce algorithm
+	lines := strings.Split(string(data), "\n")
 	var mr mapred.MapReduce
-	results := mr.Run(text)
+	results := mr.Run(lines)
+
 	// todo print your result to stdout
+	for k, v := range results {
+		fmt.Println("%s: %d\n", k, v)
+	}
 }
--- Exc_9/solution/mapred/map_reduce.go
+++ Exc_9/solution/mapred/map_reduce.go
@@ -1,6 +1,63 @@
 package mapred
 
+import (
+	"regexp"
+	"strings"
+	"sync"
+)
+
 type MapReduce struct {
 }
 
 // todo implement mapreduce
+func (mr *MapReduce) wordCountMapper(text string) []KeyValue {
+	re := regexp.MustCompile("[a-zA-Z]+")
+	words := re.FindAllString(strings.ToLower(text), -1)
+
+	var kvs []KeyValue
+	for _, word := range words {
+		if word != "" {
+			kvs = append(kvs, KeyValue{Key: word, Value: 1})
+		}
+	}
+	return kvs
+}
+
+func (mr MapReduce) wordCountReducer(key string, values []int) KeyValue {
+	sum := 0
+	for _, value := range values {
+		sum += value
+	}
+	return KeyValue{Key: key, Value: sum}
+}
+
+func (mr MapReduce) Run(input []string) map[string]int {
+	mapChan := make(chan []KeyValue, len(input))
+	var wg sync.WaitGroup
+
+	for _, line := range input {
+		wg.Add(1)
+		go func(text string) {
+			defer wg.Done()
+			mapChan <- mr.wordCountMapper(text)
+		}(line)
+	}
+
+	wg.Wait()
+	close(mapChan)
+
+	intermediate := make(map[string][]int)
+	for kkvList := range mapChan {
+		for _, kv := range kkvList {
+			intermediate[kv.Key] = append(intermediate[kv.Key], kv.Value)
+		}
+	}
+
+	result := make(map[string]int)
+	for key, values := range intermediate {
+		reducedKV := mr.wordCountReducer(key, values)
+		result[reducedKV.Key] = reducedKV.Value
+	}
+
+	return result
+}
